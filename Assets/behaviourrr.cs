417a849528a1549a91de0406fb6046e7f1943ae2
Miquel Guspí Tricas <miquel.guspi@enti.cat>
2019-11-08 12:11:00 +0100

    master classe

---
+++ Assets/behaviourrr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class behaviourrr : MonoBehaviour
+{
+    public void Changetext(TextMeshProUGUI textUGUI)
+    {
+        textUGUI.text = "hola";
+    }
+
+    public void ChangeColor(Image img)
+    {
+        img.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
---
+++ Assets/heloo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+
+public class heloo : MonoBehaviour
+{
+    private UnityEvent OnpressSpace;
+    [SerializeField] private behaviourrr behav;
+    [SerializeField] private Image img;
+    [SerializeField] private TextMeshProUGUI text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnpressSpace = new UnityEvent();
+        OnpressSpace.AddListener(delegate { behav.ChangeColor(img); });
+        OnpressSpace.AddListener(delegate { behav.Changetext(text); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            OnpressSpace.Invoke();
+        }
+    }
+}
